6ab983f1d7a791a238cd2c5e457033fd48164541
Dustin Cintron <D.Cint88@gmail.com>
2018-10-16 12:54:47 -0400

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,19 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
+
+		# config.load_defaults 5.1 
+		# initializes configuration defaults for originally generated Rails version.
+
+		# config.eager_load_paths << "#{Rails.root}/lib"
+		# Makes it so that, upon loading the app root directory (the main dir), it loads up the /lib
+		# along with any modules you have inside it.
+		# AKA pass in the path Rails.root/lib and make it available to the entire application.
+
+    # Settings in config/environments/* take precedence over those specified in class Application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
