c3a6d6d99a9c8ae8f7ea54b58aaf2e76e9df9f55
Dustin Cintron <D.Cint88@gmail.com>
2019-09-20 21:28:34 -0400

    Updated app deploy helper method to include React Store app

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -13,13 +13,15 @@ module PortfoliosHelper
   	end
 	end
 
-  def heroku_link
+  def production_link
     if @portfolio_item.title === "Cintron Capstone Project"
       link_to image_tag(portfolio_img(@portfolio_item.main_image, 'main'), width: '100%'), "https://dmc-capstone.herokuapp.com/", target: '_blank'
     elsif @portfolio_item.title === "Overtime Application"
       link_to image_tag(portfolio_img(@portfolio_item.main_image, 'main'), width: '100%'), "https://cintron-overtime.herokuapp.com/", target: '_blank'
-    else
+    elsif @portfolio_item.title === "Freelancer Dashboard"
       link_to image_tag(portfolio_img(@portfolio_item.main_image, 'main'), width: '100%'), "https://cintron-freelance.herokuapp.com/", target: '_blank'
+    else
+      link_to image_tag(portfolio_img(@portfolio_item.main_image, 'main'), width: '100%'), "https://cintron-react-store.netlify.com/", target: '_blank'      
     end
   end
 end
